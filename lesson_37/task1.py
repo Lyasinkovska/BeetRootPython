c45ad00d75ec5987a829758a0db58ed9ab1ee6bc
Lyasinkovska <l.yasinkovska@gmail.com>
2021-03-20 11:32:44 +0200

    task_1 connection to db and basic commands

--- lesson_37/task1.py
+++ lesson_37/task1.py
@@ -3,17 +3,29 @@ from sqlite3 import Error
 
 
 def create_connection():
-
     conn = None
     try:
-        conn = sqlite3.connect(':memory:')
+        conn = sqlite3.connect('my_tb.db')
         print(sqlite3.version)
     except Error as e:
         print(e)
-    finally:
-        if conn:
-            conn.close()
+    return conn
+    # finally:
+    #     if conn:
+    #         conn.close()
+
+
+def create_cursor(connection):
+    return connection.cursor()
+
+
+def create_table(cursor, table_name: str):
+    cursor.execute(
+        f'CREATE TABLE IF NOT EXISTS {table_name}(id INTEGER NOT NULL PRIMARY KEY, fisrtname TEXT ,lastname TEXT)')
 
 
 if __name__ == '__main__':
-    create_connection()
+    connection = create_connection()
+    cursor = create_cursor(connection)
+    create_table(cursor, 'my_table')
+    connection.commit()
